fdefdef294dcd83a1e21f1e88b2e2ae5e4415cd1
Raquel Belfort <raquelzinha.belfort@gmail.com>
2020-09-07 17:32:42 -0300

    Adição de resolução.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace exercicio6_asciiart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("             .#############. ");
+            Console.WriteLine("          .###################. ");
+            Console.WriteLine("       .####%####################.,::;;;;;;;;;;, ");
+            Console.WriteLine("      .####%###############%######:::;;;;;;;;;;;;;, ");
+            Console.WriteLine("      ####%%################%######:::;;;;;;;;@;;;;;;, ");
+            Console.WriteLine("      ####%%################%%#####:::;;;;;;;;;@;;;;;;, ");
+            Console.WriteLine("      ####%%################%%#####:::;;;;;;;;;@@;;;;;; ");
+            Console.WriteLine("      `####%################%#####:::;;;;;;;;;;@@;;;;;; ");
+            Console.WriteLine("        `###%##############%####:::;;;;;;;;;;;;@@;;;;;; ");
+            Console.WriteLine("           `#################'::%%%%%%%%%%%%;;;@;;;;;;' ");
+            Console.WriteLine("             `#############'.%%%%%%%%%%%%%%%%%%;;;;;' ");
+            Console.WriteLine("               `#########'%%%%#%%%%%%%%%%%%%%%%%%%, ");
+            Console.WriteLine("                 `#####'.%%%%#%%%%%%%%%%%%%%#%%%%%%, ");
+            Console.WriteLine("                   `##' %%%%##%%%%%%%%%%%%%%%##%%%%% ");
+            Console.WriteLine("                   ###  %%%%##%%%%%%%%%%%%%%%##%%%%% ");
+            Console.WriteLine("                    '   %%%%##%%%%%%%%%%%%%%%##%%%%% ");
+            Console.WriteLine("                   '    `%%%%#%%%%%%%%%%%%%%%#%%%%%' ");
+            Console.WriteLine("                  '       `%%%#%%%%%%%%%%%%%#%%%%' ");
+            Console.WriteLine("                  `         `%%%%%%%%%%%%%%%%%%' ");
+            Console.WriteLine("                   `          `%%%%%%%%%%%%%%' ");
+            Console.WriteLine("                    `           `%%%%%%%%%%'  ' ");
+            Console.WriteLine("                     '            `%%%%%%'   ' ");
+            Console.WriteLine("                    '              `%%%'    ' ");
+            Console.WriteLine("                   '               .%%      ` ");
+            Console.WriteLine("                  `                %%%       ' ");
+            Console.WriteLine("                   `                '       ' ");
+            Console.WriteLine("                    `              '      ' ");
+            Console.WriteLine("                    '            '      ' ");
+            Console.WriteLine("                   '           '       ` ");
+            Console.WriteLine("                  '           '        ' ");
+            Console.WriteLine("                              `       ' ");
+            Console.WriteLine("a@@@@@@@@a  a@@@@@@a  a@@@@@@@a a@@@@@@@@a a@@a.  .a@@a  a@@a ");
+            Console.WriteLine("@@@@  @@@@ @@@@  @@@@ @@@@  @@@ @@@@@@@@@@ @@@@a  a@@@@  @@@@ ");
+            Console.WriteLine("@@@@  @@@@ @@@@  @@@@ @@@@  @@@    @@@@    `@@@@  @@@@'  @@@@ ");
+            Console.WriteLine("@@@@@@@@@@ @@@@@@@@@@ @@@@@@@@'    @@@@      `@@@@@@'    @@@@ ");
+            Console.WriteLine("@@@@@@@@@' @@@@@@@@@@ @@@@@@@@a    @@@@        @@@@      `@@' ");
+            Console.WriteLine("@@@@       @@@@  @@@@ @@@@ @@@@    @@@@        @@@@ ");
+            Console.WriteLine("@@@@       @@@@  @@@@ @@@@ @@@@    @@@@        @@@@       @@");
+        }
+    }
+}
